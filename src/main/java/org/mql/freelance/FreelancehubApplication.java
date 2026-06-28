f45426102881dae82ba2d5113ce390acd1c3cfb0
KEBIR <hamzakebir@gmail.com>
2026-06-28 16:56:10 +0100

    chore(config): configure datasource, JPA and Thymeleaf properties  Ref: FH-003

---
+++ src/main/java/org/mql/freelance/FreelancehubApplication.java
@@ -0,0 +1,13 @@
+package org.mql.freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mql/freelance/FreelancehubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mql.freelance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
